aea1daf1993092c02ff58e1ae68afadb03615858
david <david@multikaos.be>
2011-12-14 18:36:16 +0100

    Exercise (3.5)1

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -42,4 +42,16 @@ describe PagesController do
                                   :content => "Ruby on Rails Tutorial Sample App | About")
   end
 
+  describe "GET 'help'" do
+    it "returns http success" do
+      get 'help'
+      response.should be_success
+    end
+  end
+
+  it "should have the right title" do
+    get 'help'
+    response.should have_selector("title",
+                                  :content => "Ruby on Rails Tutorial Sample App | Help")
+  end
 end
