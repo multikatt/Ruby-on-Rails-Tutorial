8f407f0ea514bb453bc2593092310ac8c91ee5c5
David Hjelm <david@multikaos.be>
2011-12-17 15:09:31 +0100

    Finished layout and routes

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -22,4 +22,8 @@ describe "LayoutLinks" do
     response.should have_selector('title', :content => "Help")
   end
 
+  it "should have a Signup page at '/signup'" do
+    get '/signup'
+    response.should have_selector('title', :content => "Sign up")
+  end
 end
